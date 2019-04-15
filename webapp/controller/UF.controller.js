5ec0d0a713e4e271c2d81f9a2290669b2970146a
matusca96 <gomesloud@gmail.com>
2019-04-15 16:09:52 -0300

    Padronização.

--- webapp/controller/UF.controller.js
+++ webapp/controller/UF.controller.js
@@ -1,12 +1,25 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageBox"
-], function(Controller, MessageBox) {
+	"sap/m/MessageBox",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+], function(Controller, MessageBox, Filter, FilterOperator) {
 	"use strict";
 
 	return Controller.extend("br.com.idxtecUF.controller.UF", {
 		onInit: function(){
 			this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+		},
+		
+		filtraUf: function(oEvent){
+			var sQuery = oEvent.getParameter("query");
+			var oFilter = new Filter("Sigla", FilterOperator.Contains, sQuery);
+			
+			var aFilters = [
+				oFilter
+			];
+
+			this.getView().byId("tableUF").getBinding("rows").filter(aFilters, "Application");
 		},
 		
 		onRefresh: function(){
